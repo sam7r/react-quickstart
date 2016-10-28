3767137a144fb27c89de2c40076b4aa87019039f
Samuel J Roberton <design@sjroberton.co.uk>
2016-10-28 14:50:45 +0100

    Added test to App

--- app/containers/App/tests/App.test.js
+++ app/containers/App/tests/App.test.js
@@ -1,5 +1,6 @@
 import test from 'blue-tape';
-import { mount } from 'enzyme';
+import React from 'react';
+import { mount, shallow } from 'enzyme';
 import { componentSetup } from 'boilerplate/test-utils';
 import App from 'containers/App';
 
@@ -9,5 +10,13 @@ const props = {};
 test('App renders', t => {
   const component = mount(componentSetup(App, props));
   t.ok(!!component.render().find('#app-container').length,'app container rendered');
+  t.end();
+});
+
+/** @test {App} */
+test('App renders children', t => {
+  const child = <h1>App Container</h1>;
+  const component = shallow(<App>{ child }</App>);
+  t.ok(component.contains(child),'app container rendered children');
   t.end();
 });
